522ed578f4f74c0ae78f93fb6b5518dc734e4783
mehrabix <mehrabi@post.com>
2021-03-26 12:35:22 +0430

    //

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -10,7 +10,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
   } className={`${size} menu-item`}>
     <div className="content">
       <h1 className="title">{title}</h1>
-      <span className="subtitle"></span>
+      <span className="subtitle">Shop NOW</span>
     </div>
   </div>
 );
