02d349c96e0056bb213dcb2986e6eea77a459384
mehrabix <mehrabi@post.com>
2021-06-29 11:10:23 +0430

    add login form and styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -27,13 +27,11 @@ class SignIn extends React.Component {
         return (
             <div className="sign-in">
                 <h2>I already have an account!</h2>
-                <span>Sign in with your email</span>
+                <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input type='email' name="email" value={this.state.email} onChange={this.handleChange} required />
-                    <label>Email</label>
-                    <input type='password' name="password" value={this.state.password} onChange={this.handleChange} required />
-                    <label>Password</label>
+                    <FormInput type='email' name="email" value={this.state.email} label='email' handleChange={this.handleChange} required />
+                    <FormInput type='password' name="password" value={this.state.password} label='password' handleChange={this.handleChange} required />
                     <input type="submit" value='Submit Form' />
                 </form>
             </div>
--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
 import SignIn from '../../components/sign-in/sign-in.component';
 import './signin-and-signup.component.scss';
 
 const SignInAndSignup = () => (
-    <div className="Sign-in-And-Sign-up"><SignIn></SignIn></div>
+    <div className="Sign-in-And-Sign-up">
+
+        <SignIn />
+
+    </div>
 )
 
 export default SignInAndSignup;
